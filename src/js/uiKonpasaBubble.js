5062aa05472df22a14215798ce1c4e6340cc2ca1
Heyde Moura <moura.heyde@gmail.com>
2016-08-01 16:28:12 -0300

    Fixed not dismissing bubbles

--- src/js/uiKonpasaBubble.js
+++ src/js/uiKonpasaBubble.js
@@ -1,62 +1,69 @@
 angular.module('uiKonpasa')
 
-.directive('uiKonpasaBubbleArea', uiKonpasaBubbleArea)
-.directive('uiKonpasaBubble', uiKonpasaBubble);
+		.directive('uiKonpasaBubbleArea', uiKonpasaBubbleArea)
+    .directive('uiKonpasaBubble', uiKonpasaBubble);
 
 function uiKonpasaBubbleArea() {
-	return {
-		scope: {
-			bubbles : '=',
-			max : '='
-		},
-		controller : ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-			this.pushBubble = function(bubble)
-			{
-				$scope.bubbles.push(bubble);
-				delete $scope.bubble;
-			};
+    return {
+        scope: {
+            bubbles: '=',
+            max: '='
+        },
+        controller: ['$scope', '$element', '$attrs',
+            function($scope, $element, $attrs) {
+                this.pushBubble = function(bubble) {
+                    $scope.bubbles.push(bubble);
+                    delete $scope.bubble;
+                };
 
-			this.bubblePop = function(index) {
-				if (!index) {
-					$scope.bubbles.shift();
-					return;
-				}
+                this.bubblePop = function(index) {
+                    if (!index) {
+                        $scope.bubbles.shift();
+                        return;
+                    }
 
-				$scope.bubbles.splice(index, 1);
-			};
+                    $scope.bubbles.splice(index, 1);
+                };
 
-			$element.addClass('bubbleArea');
+                $element.addClass('bubbleArea');
 
-			$scope.$watch('bubbles', function(newVal, oldVal){
-				if (newVal.length > oldVal.length) {
-				    if (newVal.length -1 >= $scope.max) {
-				    	$scope.bubbles.shift();
-				    };
+                $scope.$watch('bubbles', function(newVal, oldVal) {
+                    if (newVal.length > oldVal.length) {
+                        if (newVal.length - 1 >= $scope.max) {
+                            $scope.bubbles.shift();
+                        };
 
-			    	setTimeout(function() {
-			    		$scope.bubbles.shift();
-			    	}, 2000)
-				}
-			}, true);
-		}]
-	};
+                        if ($scope.bubbles.length > 0) {
+	                        setTimeout(function() {
+	                            $scope.bubbles.shift();
+			                        $scope.$apply();
+	                        }, 2000)
+                        }
+
+                    }
+                }, true);
+            }
+        ]
+    };
 };
 
 function uiKonpasaBubble() {
-	return {
-		templateUrl: 'templates/bubble.tpl.html',
-		replace: true,
-		require: "^uiKonpasaBubbleArea",
-		scope: {
-			body: '=',
-			type: '='
-		},
-		link: ['$scope', '$element', '$attrs', '$ctrl' ,function($scope, $element, $attrs, $ctrl) {
-			$scope.close = $ctrl.bubblePop;
+    return {
+        templateUrl: 'templates/bubble.tpl.html',
+        replace: true,
+        require: "^uiKonpasaBubbleArea",
+        scope: {
+            body: '=',
+            type: '='
+        },
+        link: ['$scope', '$element', '$attrs', '$ctrl',
+            function($scope, $element, $attrs, $ctrl) {
+                $scope.close = $ctrl.bubblePop;
 
-			$element.show();
+                $element.show();
 
-			$scope.close = $element.remove;
-		}]
-	}
+                $scope.close = $element.remove;
+            }
+        ]
+    }
 }
